33dc33bd4a8dda745b0ec497d3508562031ebae7
deepali04 <nagwade.deepali@gmail.com>
2024-08-23 22:20:31 -0400

    Content microservice structure

---
+++ content-service/src/main/java/com/personal/content/repository/ContentRepository.java
@@ -0,0 +1,5 @@
+package main.java.com.personal.content.repository;
+
+public class ContentRepository {
+    
+}
---
+++ content-service/src/main/java/com/personal/content/service/ContentService.java
@@ -0,0 +1,5 @@
+package main.java.com.personal.content.service;
+
+public class ContentService {
+    
+}
